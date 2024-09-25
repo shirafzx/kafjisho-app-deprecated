c440f698a4ceecd300c772a0dc47925d5dd6a610
Shirafzx <shirafzx@gmail.com>
2024-09-25 20:41:19 +0700

    :sparkles: basic hero section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import HeroSection from "@/components/home/HeroSection";
 import SearchSection from "@/components/home/SearchSection";
 import Container from "@/components/shared/container/Container";
 
@@ -7,7 +8,7 @@ export default function Home() {
   return (
     <Container className="gap-y-16">
       <SearchSection />
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, magnam!
+      <HeroSection />
     </Container>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+                href="https://legacy.shirafzx.xyz/"
                 title="nextui.org homepage"
               >
                 <span className="text-default-600">Powered by</span>
